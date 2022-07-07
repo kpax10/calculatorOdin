c7e32fb9906b47a1e7baf761b4a9192bb8590e7a
Kyle <notxap55@yahoo.com>
2022-07-07 15:49:42 -0700

    input functions working somewhat
    needed: update the returned value to displayed value

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict;'
 
-const add = (a, b) => a + b;
+const add = (a, b) => Number(a) + Number(b);
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
@@ -14,13 +14,15 @@ const operate = function (operator, a, b) {
 
 // console.log(operate('/', 1, 3));
 
-const buttonNumbers = document.querySelectorAll('#btn-num');
+const numberButtons = document.querySelectorAll('#btn-num');
 const display = document.querySelector('#display-numbers');
 
 let displayValue = '';
+let userInput = '';
+let operator = '';
 
-// loop over buttonNumbers and add event listener to buttons creating the displayValue
-buttonNumbers.forEach(element => {
+// loop over numberButtons and add event listener to buttons creating the displayValue
+numberButtons.forEach(element => {
   element.addEventListener('click', function (e) {
     if (!(displayValue.length > 10)) {
       displayValue += e.target.textContent;
@@ -29,3 +31,21 @@ buttonNumbers.forEach(element => {
   })
 });
 
+// loop over operator buttons, storing the operator clicked
+// once operator is clicked, displayValue = userInput
+const operatorButtons = document.querySelectorAll('.btn-operator');
+
+operatorButtons.forEach(element => {
+  element.addEventListener('click', function (e) {
+    userInput = displayValue;
+    displayValue = '';
+    operator = e.target.textContent;
+    console.log(operator);
+  })
+});
+
+const equalsButton = document.querySelector('.btn-equals');
+
+equalsButton.addEventListener('click', function () {
+  console.log(operate(operator, userInput, displayValue))
+});
